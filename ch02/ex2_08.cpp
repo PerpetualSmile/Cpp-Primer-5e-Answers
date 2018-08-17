0d4f090c9943d033c1bdf41598ec1459ada6559d
PerpetualSmile <shenjundong1996@gmail.com>
2018-08-17 19:56:34 +0800

    8.17

---
+++ ch02/ex2_08.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "\x32\t\x4d\n";
+}
---
+++ ch02/ex2_39.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+struct Foo { /* */ };
+int main() {
+    return 0;
+}
